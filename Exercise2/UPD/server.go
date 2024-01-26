6e7cce9d2cf65a1ce7aa3199badf8fb0931bb1e8
Ivar Hansgard <ivarhan@stud.ntnu.no>
2024-01-26 18:49:51 +0100

    done some work on the TCP and UDP server

--- Exercise2/UPD/server.go
+++ Exercise2/UPD/server.go
@@ -6,12 +6,50 @@ import (
 	. "fmt"
 	"log"
 	"net"
+	"runtime"
+	"time"
 )
 
 func listenUDPport(port string) string {
 	recieveBuffer := make([]byte, 1024)
 
-	udpAddress, err := net.ResolveUDPAddr("udp", ":"+port)
+	udpAddress, err := net.ResolveUDPAddr("udp", port)
+
+	udpConnection, err := net.ListenUDP("udp", udpAddress)
+
+	numBytes, ipAddr, err := udpConnection.ReadFromUDP(recieveBuffer)
+	Println(ipAddr.IP)
+	if err != nil {
+		log.Fatal(err, numBytes)
+	}
+
+	return ipAddr.IP.String()
+}
+
+func sendUDP(ipAdress, port, message string) {
+	udpAddress, err := net.ResolveUDPAddr("udp", ipAdress+port)
+	udpConnection, err := net.ListenPacket("udp", ":0")
+
+	numBytes, err := udpConnection.WriteTo([]byte(message), udpAddress)
+
+	if err != nil {
+		log.Fatal(err, numBytes)
+	}
+
+	Println("You sent: ", message, " Containing: ", numBytes, " bytes to IP: ", udpAddress.IP.String()+":", udpAddress.Port)
+
+	udpConnection.Close()
+}
+
+func broadcastdUDP(ipAdress, port, message string) {
+	/*usikker om det finnes*/
+}
+
+func readUDP(ipAdress, port string, buffer chan []byte, n chan int) {
+
+	recieveBuffer := make([]byte, 1024)
+
+	udpAddress, err := net.ResolveUDPAddr("udp", ipAdress+port)
 
 	udpConnection, err := net.ListenUDP("udp", udpAddress)
 
@@ -21,23 +59,83 @@ func listenUDPport(port string) string {
 		log.Fatal(err, numBytes)
 	}
 
-	return ipAddr.IP.String()
+	Println("you recieved: ", numBytes, "bytes from IP: ", ipAddr)
+
+	buffer <- recieveBuffer
+	n <- numBytes
+
+	udpConnection.Close()
 }
 
-func sendUDP() {
-	recieveBuffer := make([]byte, 1024)
+func sendTCP(ipAdress, port, message string) {
+	tcpAdress, err := net.ResolveTCPAddr("tcp", ipAdress+port)
+	tcpConnection, err := net.DialTCP("tcp", nil, tcpAdress)
 
-	udpAddress, err := net.ResolveUDPAddr("udp", "10.100.23.186:20023")
-	udpConnection, err := net.ListenPacket("udp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	numBytes, err := udpConnection.WriteTo([]byte("HEI"), udpAddress)
+	numBytes, err := tcpConnection.Write([]byte(message))
+	tcpConnection.CloseWrite()
 
 	if err != nil {
 		log.Fatal(err, numBytes)
 	}
-	Print(numBytes)
+
+}
+
+func readTCP(ipAdress, port string) {
+	recieveBuffer := make([]byte, 1024)
+
+	tcpAdress, err := net.ResolveTCPAddr("tcp", ipAdress+port)
+	tcpConnection, err := net.DialTCP("tcp", nil, tcpAdress)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	numBytes, err := tcpConnection.Read(recieveBuffer)
+	tcpConnection.CloseRead()
+
+	if err != nil {
+		log.Fatal(err, numBytes)
+	}
+	Println(recieveBuffer[0:numBytes])
 }
 
 func main() {
 
+	runtime.GOMAXPROCS(2)
+
+	//test listen udp to get server ip
+	//listenUDPport(":30000")
+
+	/* Test send/recieve UPD
+	buffer := make(chan []byte, 1024)
+	n := make(chan int)
+
+	for {
+		go sendUDP("192.168.1.210", ":20023", "HEI")
+		go readUDP("192.168.1.210", ":20023", buffer, n)
+		select {
+		case x := <-buffer:
+			y := <-n
+			print("Message recieved was: ")
+			for i := 0; i < y; i++ {
+				Printf("%c", x[i])
+			}
+			print("\n")
+		}
+		time.Sleep(5 * time.Second)
+	}
+	*/
+
+	///* Test send/recieve TCP
+	for {
+		//setup channels like udp ^^
+		sendTCP("192.168.1.210", ":34933", "Connect to: 192.168.1.210")
+		readTCP("192.168.1.210", ":34933")
+		time.Sleep(5 * time.Second)
+	}
+	//*/
 }
