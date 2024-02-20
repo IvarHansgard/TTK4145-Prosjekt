9b0515e43c254aee64b25e95e65ce2407b0315f6
Ivar Hansgard <ivarhan@stud.ntnu.no>
2024-02-20 13:04:11 +0100

    Oving4 done

--- Exercise4/main.go
+++ Exercise4/main.go
@@ -10,59 +10,6 @@ import (
 	"time"
 )
 
-/*
-func mainA() {
-
-	number := 0
-	/*
-		f, err := os.Create("./backup.txt")
-
-		if err != nil {
-			panic(err)
-		}
-
-		f.Close()
-
-	dat, err := os.Open("./backup.txt")
-
-	if err != nil {
-		panic(err)
-	}
-
-	//fileReader := bufio.NewReader(dat)
-	//fileWriter := bufio.NewWriter(dat)
-	//file := bufio.NewReadWriter(fileReader, fileWriter)
-	n, err := dat.Write([]byte("string"))
-	//n, err = fileWriter.WriteString("0string(number)\n")
-	fmt.Println(n)
-	/*
-		s, err := fileReader.ReadBytes('\n')
-		number, err = strconv.Atoi(string(s))
-*/
-/*
-		if err != nil {
-			panic(err)
-		}
-
-	for i := 0; i < 6; i++ {
-		fmt.Println(number)
-		number = number + 1
-		time.Sleep(1 * time.Second)
-	}
-}
-*/
-// funksjon for A
-func mainA() {
-
-	number := 0
-
-	for i := 0; i < 6; i++ {
-		fmt.Println(number)
-		number = number + 1
-		time.Sleep(1 * time.Second)
-	}
-}
-
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -73,6 +20,7 @@ func main() {
 	number := 0
 
 	if _, err := os.Stat("backup"); err == nil {
+		// path/to/whatever exists
 
 		for {
 			info, err := os.Stat("backup")
@@ -83,42 +31,50 @@ func main() {
 			if t == info.ModTime().Second()+3 {
 				break
 			}
-
-			var lastline string
-
-			f, err := os.Open("backup")
-			check(err)
-			scanner := bufio.NewScanner(f)
-
-			for scanner.Scan() {
-				lastline = scanner.Text()
-			}
-
-			n, err := strconv.Atoi(lastline)
-			check(err)
-			number = n
-			exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
 		}
+
+		var lastline string
+
+		f, err := os.Open("backup")
+		check(err)
+		scanner := bufio.NewScanner(f)
+
+		for scanner.Scan() {
+			lastline = scanner.Text()
+		}
+
+		n, err := strconv.Atoi(lastline)
+		check(err)
+		number = n
+		//exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
 	} else if errors.Is(err, os.ErrNotExist) {
-		f, err := os.Create("backup")
+		// path/to/whatever does *not* exist
+		f, err := os.OpenFile("backup", os.O_CREATE, 0600)
 		check(err)
 
 		defer f.Close()
 
 		f.Sync()
 
-		exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
+		//exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
+
+	} else {
+		fmt.Println("ERROR!!")
 	}
-	f, err := os.Open("backup")
+
+	f, err := os.OpenFile("backup", os.O_WRONLY|os.O_APPEND, 0600)
 	check(err)
 
 	w := bufio.NewWriter(f)
 
-	for i := number; i < i+10; i++ {
+	exec.Command("cmd", "/C", "start", "powershell", "go", "run", "main.go").Run()
+	//exec.Command("gnome-terminal", "--", "go", "run", "main.go").Run()
+
+	for i := number; i < number+10; i++ {
 		s := fmt.Sprint(i, "\n")
 		n, err := w.WriteString(s)
 		w.Flush()
-		print(n)
+		fmt.Println("wrote: ", n, " bytes", " number: ", i)
 		check(err)
 		time.Sleep(1 * time.Second)
 	}
